aaf8d121c0a023d5bfdc66a2addfef5d4caaed4a
baezluise <107555064+baezluise@users.noreply.github.com>
2023-04-17 10:47:17 -0500

    Fix linters
    Single quotes used
    Line limit changed to 100

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // JavaScript code to show/hide mobile menu
-const mobileMenu = document.querySelector(".mobile-menu");
-const menuToggle = document.querySelector(".menu-toggle");
-const closeButton = document.querySelector(".close-button");
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuToggle = document.querySelector('.menu-toggle');
+const closeButton = document.querySelector('.close-button');
 
-menuToggle.addEventListener("click", () => {
-  mobileMenu.classList.add("show"); // add class 'show' to display the menu
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.add('show'); // add class 'show' to display the menu
 });
 
-closeButton.addEventListener("click", () => {
-  mobileMenu.classList.remove("show"); // remove class 'show' to hide the menu
+closeButton.addEventListener('click', () => {
+  mobileMenu.classList.remove('show'); // remove class 'show' to hide the menu
 });
 
 // Hide the menu when a link is clicked
-mobileMenu.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("click", () => {
-    mobileMenu.classList.remove("show"); // remove class 'show' to hide the menu
+mobileMenu.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('show'); // remove class 'show' to hide the menu
   });
 });
